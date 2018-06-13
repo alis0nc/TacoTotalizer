957fd212f5056a4687545179b1c92dab0034d546
Alison C <alisonc@alisonc.net>
2018-06-13 16:58:04 -0400

    initial commit

---
+++ src/main/java/net/alisonc/tacototalizer/TacoTotalizerApplication.java
@@ -0,0 +1,12 @@
+package net.alisonc.tacototalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoTotalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoTotalizerApplication.class, args);
+	}
+}
